d11ad48d19799da8c09d4a166ba2213787999365
Fernanda Domenicali <43413565+nandadomenicali@users.noreply.github.com>
2023-08-16 14:40:19 -0300

    (feat): importing helper on app js

--- app.mjs
+++ app.mjs
@@ -1,9 +1,12 @@
 import express from 'express';
+import connectToDatabase from './helper.mjs';
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('<h2>Hi there!</h2>')
 });
+
+await connectToDatabase();
 
 app.listen(3000);
